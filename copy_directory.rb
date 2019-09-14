756c84fcd8ca778ab30e7ad6cd0c000974f66d07
yasmine.morawej@gmail.com <yasmine.morawej@gmail.com>
2019-09-14 16:55:27 +0100

    take arguments from the command line

--- copy_directory.rb
+++ copy_directory.rb
@@ -1,35 +1,4 @@
-@students = [] # empty array accessible to all methods
-
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
-  name = gets.chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do
-    puts "Cohort?"
-    cohort = gets.chomp.downcase
-    while !months.include?(cohort)
-      puts "Month not valid. Re-enter"
-      cohort = gets.chomp.downcase
-    end
-   @students << {name: name, cohort: (cohort.to_sym)}
-    if @students.count == 1 
-      puts "Now we have #{@students.count} student"
-    else
-      puts "Now we have #{@students.count} students"
-    end  
-    # get another name from the user
-    name = gets.chomp
-  end   
-end
-
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
-  end
-end 
+@students = []
 
 def print_menu
   puts "1. Input the students"
@@ -39,11 +8,12 @@ def print_menu
   puts "9. Exit"
 end  
 
-def show_students
-  print_header
-  print_students_list
-  print_footer
-end  
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
+end 
 
 def process(selection)
   case selection
@@ -62,31 +32,42 @@ def process(selection)
   end
 end
 
-def save_students
-  file = File.open("students.csv", "w")
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
-  end
-  file.close 
-end  
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+  name = STDIN.gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    puts "Cohort?"
+    cohort = gets.chomp.downcase
+    while !months.include?(cohort)
+      puts "Month not valid. Re-enter"
+      cohort = STDIN.gets.chomp.downcase
+    end
+   @students << {name: name, cohort: (cohort.to_sym)}
+    if @students.count == 1 
+      puts "Now we have #{@students.count} student"
+    else
+      puts "Now we have #{@students.count} students"
+    end  
+    # get another name from the user
+    name = STDIN.gets.chomp
+  end   
+end
 
-def load_students
-  file = File.open("students.csv", "r")
-  file.readlines.each do |line|
-  name, cohort = line.chomp.split(",")
-    @students << {name: name, cohort: cohort.to_sym}
-  end
-  file.close    
+def show_students
+  print_header
+  print_students_list
+  print_footer
 end  
 
  def print_header
    puts "The students of Villains Academy".center(50)
    puts "-------------".center(50)
- end	
+ end
 
-def print_students_list
+ def print_students_list
   @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)".center(50)
   end  
@@ -97,8 +78,41 @@ def print_footer
   puts "Overall, we have #{@students.count} great students".center(50)
 end
 
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close 
+end  
 
-# my extra methods --
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close    
+end  
+
+def try_load_students
+  # first argument from command line
+  filename = ARGV.first
+  # get out of the method if it doesnn't exist 
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else 
+    puts "Sorry, #{filename} doesn't exist" 
+    exit
+  end     
+end	
+
+
+# --- my extra methods --
 def name_starts_with(names)
   puts "Which letter would you like to search?"
   letter = gets.chomp.downcase
@@ -122,6 +136,7 @@ end
 
 # --
  
+try_load_students
 interactive_menu
 
 
